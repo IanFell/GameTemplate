37332c3230d86fbb8c3210bb595fd9e7d242db95
Ian Fell <ianfell>
2018-03-20 10:04:59 -0500

    Created inventory screen class.

---
+++ MyGame-core/src/screens/InventoryScreen.java
@@ -0,0 +1,22 @@
+package screens;
+
+import com.mygdx.mygame.MyGame;
+
+/**
+ * Screen for inventory.
+ * 
+ * @author Fabulous Fellini
+ *
+ */
+public class InventoryScreen extends Screens {
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @para mMyGame myGame
+	 */
+	public InventoryScreen(MyGame myGame) {
+		super(myGame);
+		// TODO Auto-generated constructor stub
+	}
+}
