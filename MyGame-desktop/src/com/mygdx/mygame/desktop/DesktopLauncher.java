fd0e9d32f140d3319fb1ba487b954f08a39770db
Ian Fell <ianfell>
2018-05-13 14:24:30 -0500

    Added window icon.

--- MyGame-desktop/src/com/mygdx/mygame/desktop/DesktopLauncher.java
+++ MyGame-desktop/src/com/mygdx/mygame/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.mygame.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.mygame.MyGame;
@@ -13,6 +14,7 @@ import com.mygdx.mygame.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("GrassTileOne.png", FileType.Internal);
 		new LwjglApplication(new MyGame(), config);
 	}
 }
