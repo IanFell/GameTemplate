170141b09b375d60ec66c5e0ed70c33db462cfe4
Ian Fell <ianfell>
2019-11-22 08:11:18 -0600

    Fixed placement of 'the point' sign.

--- MyGame-core/src/loaders/signloader/SignLoader.java
+++ MyGame-core/src/loaders/signloader/SignLoader.java
@@ -25,7 +25,7 @@ public class SignLoader extends ClassObjectLoader {
 		signs[7]  = new Sign("St George", GameAttributeHelper.CHUNK_SEVEN_X_POSITION_START - 18, GameAttributeHelper.CHUNK_EIGHT_Y_POSITION_START - 13);
 		signs[8]  = new Sign("Cape San Blas", GameAttributeHelper.CHUNK_THREE_X_POSITION_START - 31, GameAttributeHelper.CHUNK_SIX_Y_POSITION_START - 33);
 		signs[9]  = new Sign("Cape San Blas", GameAttributeHelper.CHUNK_THREE_X_POSITION_START - 54, GameAttributeHelper.CHUNK_SIX_Y_POSITION_START - 70);
-		signs[10] = new Sign("The Point", GameAttributeHelper.CHUNK_TWO_X_POSITION_START + 11, GameAttributeHelper.CHUNK_FIVE_Y_POSITION_START - 11);
+		signs[10] = new Sign("The Point", GameAttributeHelper.CHUNK_TWO_X_POSITION_START + 11, GameAttributeHelper.CHUNK_FIVE_Y_POSITION_START - 111);
 		signs[11] = new Sign("The Point", GameAttributeHelper.CHUNK_TWO_X_POSITION_START + 4, GameAttributeHelper.CHUNK_FIVE_Y_POSITION_START - 86);
 		signs[12] = new Sign("Port St Joe", GameAttributeHelper.CHUNK_FOUR_X_POSITION_START + 22, GameAttributeHelper.CHUNK_THREE_Y_POSITION_START + 3);
 		signs[13] = new Sign("Port St Joe", GameAttributeHelper.CHUNK_FOUR_X_POSITION_START - 1, GameAttributeHelper.CHUNK_THREE_Y_POSITION_START - 20);
