2d48f7d1ddbce03657cdcc163f0201780320b447
Ian Fell <ianfell>
2019-12-27 09:06:53 -0600

    Implemented more stumps.

--- MyGame-core/src/loaders/stumploader/StumpLoader.java
+++ MyGame-core/src/loaders/stumploader/StumpLoader.java
@@ -14,7 +14,9 @@ import loaders.ClassObjectLoader;
  */
 public class StumpLoader extends ClassObjectLoader {
 
-	private final int AMOUNT_OF_STUMPS = 1;
+	private final int AMOUNT_OF_STUMPS = 20;
+	
+	private boolean newRowShouldBeCreated = true;
 
 	public static ArrayList <GameObject> stumps = new ArrayList <GameObject>();
 
@@ -23,6 +25,13 @@ public class StumpLoader extends ClassObjectLoader {
 		int startY = GameAttributeHelper.CHUNK_SEVEN_Y_POSITION_START + 35;
 		for (int i = 0; i < AMOUNT_OF_STUMPS; i++) {
 			stumps.add(new Stump(startX, startY));
+			startX += 2;
+			// Create new row of stumps.
+			if (i > AMOUNT_OF_STUMPS / 2 && newRowShouldBeCreated) {
+				startX = GameAttributeHelper.CHUNK_FOUR_X_POSITION_START - 16;
+				startY += 4;
+				newRowShouldBeCreated = false;
+			}
 		}
 		addGameObjectsToGameObjectArrayList(stumps);
 	}
